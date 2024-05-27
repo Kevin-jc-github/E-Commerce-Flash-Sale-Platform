64b9e60806b56b98d99ecd310b8e6bc061031df1
JiancongZhu0423 <jiancoz@uci.edu>
2024-05-27 12:42:51 -0700

    Implement the timeout order function in this project 1. Implement the delaymessage in "createorder" function 2. Create "PayStatusCheckListener" class in mq package 3. Implement the OnMessage function to deal with the time out order 4. Implement the revertStock() method in  seckillActivityDao class and redisService class // revertStock() method is for the scenario when the consumer make an order but does not pay it in a certain time, the order will be cancel and the available stock will be re-add and lock stock will be -1.

--- src/main/java/com/jiuzhang/seckill/db/dao/SeckillActivityDaoImpl.java
+++ src/main/java/com/jiuzhang/seckill/db/dao/SeckillActivityDaoImpl.java
@@ -55,5 +55,10 @@ public class SeckillActivityDaoImpl implements SeckillActivityDao {
         return true;
     }
 
+    @Override
+    public void revertStock(Long seckillActivityId) {
+        seckillActivityMapper.revertStock(seckillActivityId);
+    }
+
 
 }
