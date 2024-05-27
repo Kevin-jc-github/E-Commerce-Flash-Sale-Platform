2bcb4c9c7ad12014cdba68a0ad19b09f0ec43940
JiancongZhu0423 <jiancoz@uci.edu>
2024-05-26 19:06:58 -0700

    1.Add frontend functions of order form and user form. 2.Implement the create order function 3.Use snowflake algorithm from Twitter to generate ID 4.Integrate the RocketMQ middleware into this project 5.Send the create order message by using RocketMQ 5.Deal with the order message 6.Implement the deduct stock function after received the create order message

--- src/main/java/com/jiuzhang/seckill/service/SeckillActivityService.java
+++ src/main/java/com/jiuzhang/seckill/service/SeckillActivityService.java
@@ -1,21 +1,69 @@
 package com.jiuzhang.seckill.service;
 
+import com.alibaba.fastjson.JSON;
+import com.jiuzhang.seckill.db.dao.OrderDao;
+import com.jiuzhang.seckill.db.dao.SeckillActivityDao;
+import com.jiuzhang.seckill.db.po.Order;
+import com.jiuzhang.seckill.db.po.SeckillActivity;
+import com.jiuzhang.seckill.mq.RocketMQService;
 import com.jiuzhang.seckill.util.RedisService;
+import com.jiuzhang.seckill.util.SnowFlake;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 @Service
+@Slf4j
 public class SeckillActivityService {
 
     @Resource
     private RedisService service;
+
+    @Resource
+    private SeckillActivityDao seckillActivityDao;
+
+    @Resource
+    OrderDao orderDao;
+
+    private SnowFlake snowFlake = new SnowFlake(1,1);
+
+    @Resource
+    private RocketMQService rocketMQService;
 
     public boolean seckillStockValidator(long activityId){
         String key = "stock:" + activityId;
         return service.stockDeductValidation(key);
     }
 
+    public Order createOrder(long seckillActivityId, long userId) throws Exception {
+        SeckillActivity activity = seckillActivityDao.querySeckillActivityById(seckillActivityId);
+        Order order = new Order();
+        // use snowflake algorithm to generate order Id
+        order.setOrderNo(String.valueOf(snowFlake.nextId()));
+        order.setSeckillActivityId(activity.getId());
+        order.setUserId(userId);
+        order.setOrderAmount(activity.getSeckillPrice().longValue());
+        //send create order message to the message queue
+        rocketMQService.sendMessage("seckill_order", JSON.toJSONString(order));
+        return order;
+    }
 
+    /**
+     * 订单支付完成处理
+     * @param orderNo
+     */
+    public void payOrderProcess(String orderNo) {
+        log.info("完成支付订单 订单号：" + orderNo);
+        Order order = orderDao.queryOrder(orderNo);
+        boolean deductStockResult = seckillActivityDao.deductStock(order.getSeckillActivityId());
+        if (deductStockResult) {
+            order.setPayTime(new Date());
+        // 订单状态 0、没有可用库存，无效订单 1、已创建等待支付 2、完成支付
+            order.setOrderStatus(2);
+            orderDao.updateOrder(order);
+        }
+    }
 
 }
--- src/main/java/com/jiuzhang/seckill/web/SeckillActivityController.java
+++ src/main/java/com/jiuzhang/seckill/web/SeckillActivityController.java
@@ -1,15 +1,20 @@
 package com.jiuzhang.seckill.web;
 
+import com.jiuzhang.seckill.db.dao.OrderDao;
 import com.jiuzhang.seckill.db.dao.SeckillActivityDao;
 import com.jiuzhang.seckill.db.dao.SeckillCommodityDao;
+import com.jiuzhang.seckill.db.po.Order;
 import com.jiuzhang.seckill.db.po.SeckillActivity;
 import com.jiuzhang.seckill.db.po.SeckillCommodity;
+import com.jiuzhang.seckill.service.SeckillActivityService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
 import java.text.ParseException;
@@ -19,6 +24,7 @@ import java.util.Map;
 
 
 @Controller
+@Slf4j
 public class SeckillActivityController {
 
     @Autowired
@@ -26,6 +32,12 @@ public class SeckillActivityController {
 
     @Autowired
     private SeckillCommodityDao SeckillCommodityDao;
+
+    @Autowired
+    SeckillActivityService seckillActivityService;
+
+    @Autowired
+    OrderDao orderDao;
 
     @RequestMapping("/addSeckillActivity")
     public String addSeckillActivity() {
@@ -89,5 +101,67 @@ public class SeckillActivityController {
         return "seckill_item";
     }
 
+    /**
+     * 处理抢购请求
+     * @param userId
+     * @param seckillActivityId
+     * @return
+     */
+    @RequestMapping("/seckill/buy/{userId}/{seckillActivityId}")
+    public ModelAndView seckillCommodity(@PathVariable long userId,
+                                         @PathVariable long seckillActivityId) {
+        boolean stockValidateResult = false;
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            /*
+             * 确认是否能够进行秒杀
+             */
+            stockValidateResult = seckillActivityService.seckillStockValidator(seckillActivityId);
+            if (stockValidateResult) {
+                Order order = seckillActivityService.createOrder(seckillActivityId, userId);
+                modelAndView.addObject("resultInfo","秒杀成功，订单创建中，订单ID：" + order.getOrderNo());
+                modelAndView.addObject("orderNo",order.getOrderNo());
+            } else {
+                modelAndView.addObject("resultInfo","对不起，商品库存不足");
+            }
+        } catch (Exception e) {
+            log.error("秒杀系统异常" + e.toString());
+            modelAndView.addObject("resultInfo","秒杀失败");
+        }
+        modelAndView.setViewName("seckill_result");
+        return modelAndView;
+    }
+
+    /**
+     * 订单查询
+     * @param orderNo
+     * @return
+     */
+    @RequestMapping("/seckill/orderQuery/{orderNo}")
+    public ModelAndView orderQuery(@PathVariable String orderNo) {
+        log.info("订单查询，订单号：" + orderNo);
+        Order order = orderDao.queryOrder(orderNo);
+        ModelAndView modelAndView = new ModelAndView();
+        if (order != null) {
+            modelAndView.setViewName("order");
+            modelAndView.addObject("order", order);
+            SeckillActivity seckillActivity =
+                    seckillActivityDao.querySeckillActivityById(order.getSeckillActivityId());
+            modelAndView.addObject("seckillActivity", seckillActivity);
+        } else {
+            modelAndView.setViewName("order_wait");
+        }
+        return modelAndView;
+    }
+
+    /**
+     * 订单支付
+     * @return
+     */
+    @RequestMapping("/seckill/payOrder/{orderNo}")
+    public String payOrder(@PathVariable String orderNo) throws Exception {
+        seckillActivityService.payOrderProcess(orderNo);
+        return "redirect:/seckill/orderQuery/" + orderNo;
+    }
 
 }
